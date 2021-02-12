f77a44e1214fe0f495b50587cd4f5b037494f285
Tyler Madonna <tjm159@pitt.edu>
2021-02-12 16:57:16 -0500

    Added GrantExtension nwb extension spec

--- exportToNWB.m
+++ exportToNWB.m
@@ -2,6 +2,34 @@ function exportToNWB(grant)
 %EXPORTTONWB Export a grant MDF object to NWB files
 %   Each trial is exported as a separate NWB file
 
+% Find extension file path
+[dir, ~, ~] = fileparts(which(mfilename));
+extFile = fullfile(dir, 'spec', 'ndx-rnel-metadata.namespace.yaml');
+if ~isfile(extFile)
+    error('Extension file ''ndx-rnel-metadata.namespace.yaml'' does not exist')
+end
+
+disp('Generating extension classes...');
+
+% Generate the extension classes
+generateExtension(extFile);
+
+disp('Exporting data...');
+
+% Grant extension class
+grantExt = types.ndx_rnel_metadata.GrantExtension( ...  
+    'investigators', strjoin({grant.PI, grant.COI}, ', '), ...
+    'url', grant.URL, ...
+    'project_number', grant.projectNumber, ...
+    'title', grant.officialName, ...
+    'alt_names', strjoin([grant.alternativeNames, {grant.name}, {grant.description.name}], ', '), ...
+    'acknowledgements', grant.description.acknowledgements, ...
+    'additional_links', grant.description.additional_links, ...
+    'funding', grant.description.funding, ...
+    'start_date', grant.startDate, ... % TODO: Recommended yyyydate format
+    'end_date', grant.endDate ... % TODO: Recommended date format
+);
+
 % for i = 1:length(grant.subjects(1).trials)
 for i = 1:2
     subject = types.core.Subject( ...
@@ -11,21 +39,24 @@ for i = 1:2
         'weight', [num2str(grant.subjects(1).weight) ' ' grant.subjects(1).weight_units], ...
         'description', [grant.subjects(1).name ', ' grant.subjects(1).name ' ' ], ...
         'species', 'Cat', ... % TODO: Recommends latin name?
-        'sex', grant.subjects(1).sex);
+        'sex', grant.subjects(1).sex ...
+    );
     
     % Create Nwb file
     nwb = NwbFile( ...
         'session_description', grant.description.description, ... % Data description
         'identifier', grant.subjects(1).trials(i).uuid, ... % Trial id
         'session_start_time', datetime(grant.subjects(1).trials(i).absoluteStartTime, 'InputFormat', 'yyyy-MM-dd''T''HH:mm:ss,SSSSSSSSSXXX', 'TimeZone', 'America/New_York'), ... % Trial start time
+        'general', types.untyped.Set('grant', grantExt), ... % Lab specific meta data
         'general_session_id', grant.subjects(1).uuid, ... % Session/Subject id
         'general_institution', 'University of Pittsburgh', ... % Institution (Is this in MDF?)
         'general_lab', 'Research Neural Engineering Lab', ... % Lab info (Is this in MDF?)
         'general_experiment_description', grant.description.description, ... % Data description
         'general_notes', grant.subjects(1).curation.fixes.stim_pulse_width_anode.notes, ... % Trial notes
         'general_protocol', ['IACUC ' grant.subjects(1).IACUC], ... % IACUC number
-        'general_keywords', strjoin(grant.description.keywords, ','), ... % IACUC keywords
-        'general_subject', subject);
+        'general_keywords', strjoin(grant.description.keywords, ', '), ... % IACUC keywords
+        'general_subject', subject ...
+    );
     
     % Initialize the electrode table and data
     elecParams = {'id', 'location'};
@@ -51,11 +82,13 @@ for i = 1:2
     end
         
     electrodesObjectView = types.untyped.ObjectView( ...
-        '/general/extracellular_ephys/electrodes');
+        '/general/extracellular_ephys/electrodes' ...
+    );
     electrodeTableRegion = types.hdmf_common.DynamicTableRegion( ...
         'table', electrodesObjectView, ...
         'description', 'all electrodes', ...
-        'data', (0:height(elecTbl)-1)');
+        'data', (0:height(elecTbl)-1)' ...
+    );
     
     wfSeries = types.core.ElectricalSeries( ...
         'timestamps', timestamps, ...
@@ -63,11 +96,11 @@ for i = 1:2
         'data_unit', 'volts', ... % TODO: Get actual units
         'electrodes', electrodeTableRegion, ...
         'starting_time_rate', grant.subjects(1).trials(i).engData(j).fs ...
-        );
+    );
     
     nwb.general_extracellular_ephys_electrodes = util.table2nwb(elecTbl, 'all electrodes');
     nwb.acquisition.set('WFSeries', wfSeries);
     
-    nwbExport(nwb, ['subject_' grant.subjects(1).subjectId '_trial_' grant.subjects(1).trials(i).uuid '.nwb']);
+    nwbExport(nwb, ['subject_' grant.subjects(1).subjectId '_trial_' grant.subjects(1).trials(i).uuid '.nwb']);    
 end
 
