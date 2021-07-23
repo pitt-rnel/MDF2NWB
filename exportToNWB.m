3d8e6677025499aa8c7034894b1c1283ba372971
Tyler Madonna <tjm159@pitt.edu>
2021-07-23 13:34:21 -0400

    Added exportToNWB function
    exportToNWB uses a user defined configuration file to map MDF objects to
    NWB objects

--- exportToNWB.m
+++ exportToNWB.m
@@ -1,15 +1,112 @@
-function nwbObj = exportToNWB(mdfObj, namespace, extPath)
+function nwbFile = exportToNWB(mdfObj, configFile)
 
-if ~exist('extPath', 'var')
-    extPath = '.';
+try
+    configStruct = ReadYaml(configFile);
+catch ME
+    error(['Unable to read config file: ' ME])
 end
 
-%% Generate 
-[nsFile, extFile] = generateNWBExtension(mdfObj, namespace, extPath);
-generateExtension(nsFile);
+if ~isfield(configStruct, 'description')
+    error('Config file must contain a description section')
+end
 
-metaObj = mdfObj.metadata;
-metaNwbObj = generateNWBObj(metaObj, mdfObj.type, namespace);
+if ~isfield(configStruct, 'map')
+    error('Config file must contain a map section')
+end
 
-% Just return the meta object for now
-nwbObj = metaNwbObj;
+configDescription = configStruct.description;
+
+if ~isfield(configDescription, 'filename')
+    error('Config file must contain a filename')
+end
+
+nwbFilename = configDescription.filename;
+if ~endsWith(nwbFilename, '.nwb')
+    nwbFilename = [nwbFilename '.nwb'];
+end
+
+nwbMap = containers.Map();
+configMap = configStruct.map;
+for i = 1:length(configMap)
+    map = configMap{i};
+    
+    if isfield(map, 'properties')
+        % Class is declared using the properties syntax
+        props = map.properties;
+        propFields = fields(props);
+        
+        nwbObj = feval(map.name);
+        for j = 1:length(propFields)
+            propName = propFields{j};
+            propValue = props.(propName);
+            
+            captGroups = regexp(propValue, '^\$lambda -> (.+)$', 'tokens');
+            if ~isempty(captGroups)
+                funcStr = ['@(mdfRoot) ' captGroups{1}{1}];
+                anonFunc = str2func(funcStr);
+                nwbObj.(propName) = feval(anonFunc, mdfObj);
+                continue;
+            end
+            
+            captGroups = regexp(propValue, '^\$lambda ([a-zA-Z0-9]+) -> (.+)$', 'tokens');
+            if ~isempty(captGroups)
+                funcStr = ['@(' captGroups{1}{1} ') ' captGroups{1}{2}];
+                anonFunc = str2func(funcStr);
+                nwbObj.(propName) = feval(anonFunc, mdfObj);
+                continue;
+            end
+            
+            captGroups = regexp(propValue, '^\$func -> (.+)$', 'tokens');
+            if ~isempty(captGroups)
+                funcStr = captGroups{1}{1};
+                anonFunc = str2func(funcStr);
+                nwbObj.(propName) = feval(anonFunc, mdfObj);
+                continue;
+            end
+
+        end
+        nwbMap(map.type) = nwbObj;
+        
+    elseif isfield(map, 'func')
+        % Class is declared using the func syntax
+        
+        nonIterableTypes = {'types.core.NWBFile', 'types.core.Subject'};
+        
+        if ~isfield(map, 'iterations')
+            map.iterations = 1;
+        end
+        
+        if ~exist('lazyMaps', 'var')
+            lazyMaps = map;
+        else
+            lazyMaps(length(lazyMaps) + 1) = map;
+        end
+    end
+end
+
+if isKey(nwbMap, 'types.core.NWBFile')
+    nwbFile = nwbMap('types.core.NWBFile');
+else
+    error('Config file must contain a ''types.core.NWBFile'' type')
+end
+
+if isKey(nwbMap, 'types.core.Subject')
+    nwbFile.general_subject = nwbMap('types.core.Subject');
+end
+
+if exist('lazyMaps', 'var') && ~isempty(lazyMaps)
+    
+    for i = 1:length(lazyMaps)
+        lazyMap = lazyMaps(i);
+        propertyName = lazyMap.property;
+        func = str2func(lazyMap.func);
+        for j = 1:lazyMap.iterations
+            [resultName, result] = feval(func, mdfObj, j);
+            nwbFile.(propertyName).set(resultName, result);
+            nwbExport(nwbFile, nwbFilename);
+            nwbFile = nwbRead(nwbFilename);
+        end
+    end
+end
+
+end
--- getExtensionName.m
+++
@@ -1,7 +0,0 @@
-function extName = getExtensionName(name)
-
-if endsWith(name, 'Extension')
-    extName = [upper(name(1)) name(2:end)];
-else
-    extName = [upper(name(1)) name(2:end) 'Extension'];
-end
